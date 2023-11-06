40f76e6273460c3bbc333d9d6ad655889e80fcc6
nikogoli <k_m_naoto@yahoo.co.jp>
2023-11-06 13:00:33 +0900

    変更：with-pragme ディレクトリの作成と .mod の対応

--- mod.ts
+++
@@ -1,7 +0,0 @@
-import useClosable from "./assets/hooks/useClosable.ts"
-import useRender from "./assets/hooks/useRender.ts"
-import usePopup from "./assets/hooks/usePopup.ts"
-export { useClosable, useRender, usePopup }
-
-import SimpleSpinner from "./assets/components/SimpleSpinner.tsx"
-export { SimpleSpinner }
---
+++ with-pragma/mod.ts
@@ -0,0 +1,7 @@
+import useClosable from "../hooks/useClosable.ts"
+import useRender from "../hooks/useRender.ts"
+import usePopup from "../hooks/usePopup.ts"
+export { useClosable, useRender, usePopup }
+
+import SimpleSpinner from "./components/SimpleSpinner.tsx"
+export { SimpleSpinner }
