a672089d9932cb3222c2ec1a80bb1624cca9c76c
nikogoli <k_m_naoto@yahoo.co.jp>
2025-07-09 12:18:28 +0900

    変更(spinner)：hover 時の色設定を可能に

--- no-pragma/components/SimpleSpinner.tsx
+++ no-pragma/components/SimpleSpinner.tsx
@@ -2,14 +2,20 @@ export default function SimpleSpinner(props:{
   size?: number,
   main_color?: string,
   bg_color?: string,
+  with_hover?:{
+    bg: string,
+    target: null | string
+  },
 }){
+  const { main_color, bg_color:bg, with_hover } = props
   const size = props.size ? `h-${props.size} w-${props.size}` : "h-10 w-10"
-  const main = "border-" + (props.main_color ? props.main_color : "sky-400")
-  const bg = props.bg_color ? props.bg_color : "inherit"
+  const main = "border-" + (main_color ?? "sky-400")
+  const hover_cls = with_hover ? `${with_hover.target ? with_hover.target+"-" : "" }hover:border-t-[${with_hover.bg}]` : ""
+  const bg_cls = `border-t-[${bg ?? "inherit"}] ${hover_cls}`
   return (
     <div class="flex justify-center">
-      <div class={`animate-spin ${size} border-4 ${main} rounded-full`}
-            style={{"border-top-color": bg}}></div>
+      <div class={`animate-spin ${size} border-4 ${main} rounded-full ${bg_cls}`}>
+      </div>
     </div>
   )
 }
